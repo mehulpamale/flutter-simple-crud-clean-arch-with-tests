14dd040d1d97a1bb42780c4f1fe9662889c374d3
Mehul <mehulpamale@gmail.com>
2022-11-02 12:24:27 +0530

    add local_data_source_impl_test.dart

---
+++ integration_test/feature/data/remote/data_sources/local_data_source_impl_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:product_crud_demo/enums/product_category.dart';
+import 'package:product_crud_demo/feature/data/remote/data_sources/local_data_source_impl.dart';
+import 'package:product_crud_demo/feature/data/remote/hive/models/product_hive_model.dart';
+import 'package:product_crud_demo/feature/domain/entities/product_enitity.dart';
+
+void main() {
+  group("LocalDataSourceImpl test", () {
+    setUp(() async {
+      await Hive.initFlutter();
+      Hive.registerAdapter(ProductHiveModelAdapter());
+      await Hive.openBox("products");
+    });
+
+    test("LocalDataSourceImpl should create product", () async {
+      var localDataStore = LocalDataSourceImpl();
+      var entity = ProductEntity(
+          id: "id",
+          name: "name",
+          category: ProductCategory.industrial,
+          createdAt: DateTime.now().toString());
+      await localDataStore.createProduct(entity);
+      var products = await localDataStore.getProducts();
+      expect(products[0], entity);
+    });
+  });
+}
