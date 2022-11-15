7d073a43c84df482557c857c4e23859191a341ca
Mehul <mehul.pamale@nonstopio.com>
2022-11-15 23:28:20 +0530

    test coverage | product_hive_model.g_test.dart

---
+++ test/feature/data/remote/hive/models/product_hive_model.g_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:product_crud_demo/feature/data/remote/hive/models/product_hive_model.dart';
+
+void main() {
+  test("creates ProductHiveModelAdapter", () => ProductHiveModelAdapter());
+}
